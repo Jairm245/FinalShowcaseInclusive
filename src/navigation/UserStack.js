74b575eed53a6dcc3177d6c27aa3342fbe2bad08
Ryan-Aguilar <ryanevo310@gmail.com>
2026-08-03 09:50:04 -0700

    I have a modified profile screen with some tags

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -11,6 +11,7 @@ import FriendStory from "../screens/FriendStory";
 import AstrologyScreen from "../screens/AstrologyScreen";
 import MemoryScreen from "../screens/MemoryScreen";
 import EventScreen from "../screens/EventScreen"; //New component by Sona and Christian
+import BackgroundBuild from "../screens/BackgroundBuild";
 
 const Stack = createStackNavigator();
 
@@ -68,6 +69,10 @@ export default function App() {
           name="Event"
           component={EventScreen}
           options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name="BackgroundBuild"
+          component={BackgroundBuild}
         />
       </Stack.Navigator>
     </NavigationContainer>
